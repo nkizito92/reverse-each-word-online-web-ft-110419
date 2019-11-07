e3020ad39c434bac0bb9a5725bb9389752e931c3
Kizito Njoku <nkizito57@gmail.com>
2019-11-07 14:49:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(sentence1)
+  sentence1.split(" ")
  sentence1.collect do |change|
-    "This is reverse #{change.reverse}"
+    change.reverse
  end
 end
 reverse_each_word(["Hello, there how are you?"])
