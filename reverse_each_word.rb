43817d24dbcb7172188e5975f24551619e766e7a
Kizito Njoku <nkizito57@gmail.com>
2019-11-07 15:17:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,11 @@
 
 def reverse_each_word(sentence1)
    arr = sentence1.split(" ")
+   new_arr = []
  arr.map do|change|
-   change.reverse
+   new_arr << change.reverse
  end
+ new_arr.join(" ")
 end
 
 
